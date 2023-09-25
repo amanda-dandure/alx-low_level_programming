37b4dbbff577da4d06e9eeb491be3812da2a74dd
amanda-dandure <amandadandure2@gmail.com>
2023-09-25 12:45:38 -0700

    Function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
 /**
- * _strstr - Function that locates a substring
- * @haystack: This is the string that will be searched
- * @needle: This is the substring to be located
- * Return: NULL
+ * print_chessboard - Function that prints a chess board
+ * @a: Thisis the pointer to the piece that are to be printed
+ * Return: void
  */
 
-char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
