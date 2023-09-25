ea60f7732306dd4ad0c9e8a355ef4e92c4d3a890
amanda-dandure <amandadandure2@gmail.com>
2023-09-25 12:35:05 -0700

    Function that locates a substring

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strstr - Function that locates a substring
+ * @haystack: This is the string that will be searched
+ * @needle: This is the substring to be located
+ * Return: NULL
+ */
+
+char *_strstr(char *haystack, char *needle);
