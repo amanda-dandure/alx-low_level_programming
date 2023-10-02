be8a1c6e56fddb798cdb1d7f2c569c11176d5d34
amanda-dandure <amandadandure2@gmail.com>
2023-10-02 12:35:35 -0700

    Program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name, followed by a new line
+ * @argc: This is the num of command line arg
+ * @argv: This is the array that contains the command line arg
+ * Return: (0)
+ */
+
+int main(int agrc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
