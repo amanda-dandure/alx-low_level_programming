922c84bbac55186ac9b4c66c84928096744d9b47
amanda-dandure <amandadandure2@gmail.com>
2023-10-10 07:39:33 -0700

    Defining a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+C Programming | Structures, typedef
+struct is used to define a structure 
+typedef is used to give an alias name to a data type
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a stuct that creates the dog informatio
+ * @name: this is the name of the dog
+ * @age: this is the age of the dog
+ * @owner: this is the owner of the dog
+ * Description: initial struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
