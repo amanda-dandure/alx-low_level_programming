caf7a5e3c2a4b5bd8779cc856e9c948557e977c8
amanda-dandure <amandadandure2@gmail.com>
2023-10-10 06:09:51 -0700

    Header file that defines a macro named SIZE as an abbreviation for the token 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/**
+ * File: 0-object_like_macro [related]
+ * Description: just a header file
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+C Programming | Preprocessor
+Providing inclusion of header files, macro expansions, conditional compilation, and line control.
