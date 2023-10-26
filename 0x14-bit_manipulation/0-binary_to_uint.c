ee884b08ddf92e28d8e808a07e596894536264eb
amanda-dandure <amandadandure2@gmail.com>
2023-10-26 14:07:34 -0700

    Function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,32 +6,31 @@
  * Return: the converted decimal [output]
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-unsigned int len;
-size_t i = 0;
-size_t j = 0;
-size_t sum = 0;
-size_t pow = 1;
-int base = 2;
+	unsigned int ui;
+	int len, base_two;
 
-if (b == NULL)
-return (0);
-for (len = 0; b[len] != '\0'; len++)
-;
-if (len == 1 && (b[0] == '0' || b[0] == '1'))
-{
-return (b[0] - 48);
-}
-for (i = 0; b[i] != '\0'; i++)
-{
-if (b[i] != '0' && b[i] != 1)
-return (0);
-for (j = len - 1; j > 0; j--)
-pow = pow *base;
-sum = sum + (pow * (b[i] - 48));
-len--;
-pow = 1;
-}
-return (sum);
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }
